54528d532fe4c13638ad402936c4a6f759552fbb
Ulises Aviles <ulises.aviles@cetys.edu.mx>
2020-09-17 22:22:45 -0700

    Done with bubble sort

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-let arrayOfValues = [];
-
 function setUp(docName) {
   for (let i = 0; i < 20; i++) {
     arrayOfValues[i] = i;
@@ -7,6 +5,20 @@ function setUp(docName) {
   console.log(docName);
   for (let i = 0; i < 20; i++) {
     document.getElementById(`bar${i}`).style.height = `${25 * i}px`;
+  }
+  document.getElementById("shuffle_btn").onclick = shuffle;
+  if (docName == "Bubble Sort") {
+    document.getElementById("sort_btn").onclick = bubbleSort;
+  } else if (docName == "Modified Bubble Sort") {
+    document.getElementById("sort_btn").onclick = modifiedBubbleSort;
+  } else if (docName == "Selection Sort") {
+    document.getElementById("sort_btn").onclick = selectionSort;
+  } else if (docName == "Insertion Sort") {
+    document.getElementById("sort_btn").onclick = insertionSort;
+  } else if (docName == "Quick Sort") {
+    document.getElementById("sort_btn").onclick = quickSort;
+  } else if (docName == "Merge Sort") {
+    document.getElementById("sort_btn").onclick = mergeSort;
   }
 }
 
@@ -19,35 +31,88 @@ function updateAllValues() {
 }
 
 function swapValues(index1, index2) {
+  instructionNumber++;
   aux = arrayOfValues[index1];
   arrayOfValues[index1] = arrayOfValues[index2];
   arrayOfValues[index2] = aux;
-  document.getElementById(`bar${index1}`).style.height = `${
-    25 * arrayOfValues[index1]
-  }px`;
-  document.getElementById(`bar${index2}`).style.height = `${
-    25 * arrayOfValues[index2]
-  }px`;
+  swapHistorial[instructionNumber] = [
+    [index1, arrayOfValues[index1]],
+    [index2, arrayOfValues[index2]],
+  ];
 }
 
 function run() {
   setUp(document.getElementById("page_title").innerHTML);
-  swapValues(18, 0);
-  // updateAllValues();
 }
 
-function shuffle() {}
+function shuffle() {
+  let states = [];
+  let counter = 0;
+  for (let i = 0; i < arrayOfValues.length; i++) {
+    states[i] = false;
+  }
+  for (let i = 0; counter < arrayOfValues.length; i++) {
+    while (true) {
+      randInt = Math.round(Math.random() * 19);
+      if (states[randInt] == false) {
+        states[randInt] = true;
+        arrayOfValues[i] = randInt;
+        counter++;
+        break;
+      }
+    }
+  }
+  updateAllValues();
+}
 
-function bubbleSort() {}
+function displayChanges() {
+  for (let i = 0; i < swapHistorial.length; i++) {
+    setTimeout(() => {
+      index1 = swapHistorial[i][0][0];
+      index2 = swapHistorial[i][1][0];
+      value1 = swapHistorial[i][0][1];
+      value2 = swapHistorial[i][1][1];
+      document.getElementById(`bar${index1}`).style.height = `${25 * value1}px`;
+      document.getElementById(`bar${index2}`).style.height = `${25 * value2}px`;
+    }, i * 100);
+  }
+}
 
-function modifiedBubbleSort() {}
+function bubbleSort() {
+  instructionNumber = 0;
+  for (var i = 0; i < arrayOfValues.length; i++) {
+    for (var j = 0; j < arrayOfValues.length - 1; j++) {
+      if (arrayOfValues[j] > arrayOfValues[j + 1]) {
+        swapValues(j, j + 1);
+      }
+    }
+  }
+  displayChanges();
+}
 
-function selectionSort() {}
+function modifiedBubbleSort() {
+  swapValues(2, 0);
+}
 
-function insertionSrot() {}
+function selectionSort() {
+  swapValues(3, 0);
+}
 
-function quickSort() {}
+function insertionSort() {
+  swapValues(4, 0);
+}
 
-function mergeSort() {}
+function quickSort() {
+  swapValues(5, 0);
+}
 
+function mergeSort() {
+  swapValues(6, 0);
+}
+
+window.onload = run;
+let arrayOfValues = [];
+let instructionNumber;
+let swapHistorial = [];
+let copyOfValues;
 run();
